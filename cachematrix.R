26b34ec6d683250ca3cdeed928b6801c01ee8a4a
Somal Vora <somalvora@hotmail.com>
2015-08-22 15:21:27 -0700

    Resubmitting Cache Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -10,11 +10,11 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
+  setinverse <- function(mean) m <<- mean
+  getinverse <- function() m
   list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
@@ -27,9 +27,14 @@ makeCacheMatrix <- function(x = matrix()) {
 #then the cachesolve would retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setinverse(m)
+  m
 
-  
-  
-  
-  
 }
